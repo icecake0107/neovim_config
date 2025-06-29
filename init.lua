44a76a84f32ae94b316ee3e366b72a10ee42a16d
josh <493302+icecake0107@users.noreply.github.com>
2025-06-28 18:50:06 -0700

    switch to use vtsls

--- init.lua
+++ init.lua
@@ -677,6 +677,9 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        -- ts_ls = {},
+        vtsls = {},
         vue_ls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -684,8 +687,6 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
         --
 
         lua_ls = {
