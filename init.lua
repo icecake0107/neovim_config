7b16005c34e5e85c398921d0708339b207c9c9b4
josh <493302+icecake0107@users.noreply.github.com>
2025-07-24 13:32:21 -0700

    add shortcut for jumping to the next hunk

--- init.lua
+++ init.lua
@@ -441,6 +441,32 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']g', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end, { desc = 'Jump to next git hunk' })
+
+        map('n', '[g', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end, { desc = 'Jump to previous git hunk' })
+      end,
     },
   },
 
