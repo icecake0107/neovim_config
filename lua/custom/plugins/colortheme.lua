acc6729ef567b1df3a709fc93442b8993ccd5f20
josh <493302+icecake0107@users.noreply.github.com>
2025-09-22 23:54:13 -0700

    Update colortheme.lua

--- lua/custom/plugins/colortheme.lua
+++ lua/custom/plugins/colortheme.lua
@@ -1,4 +1,20 @@
 return {
-  'yorickpeterse/nvim-grey',
+  { 'savq/melange-nvim' },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000, -- Ensure it loads early
+    opts = {
+      flavour = 'latte', -- Or "latte", "frappe", "macchiato"
+      color_overrides = {
+        latte = {},
+        frappe = {},
+        macchiato = {},
+        mocha = {},
+      },
+      custom_highlights = function(colors)
+        return {}
+      end,
+    },
+  },
 }
-
