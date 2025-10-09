d1536fcb6d5970c4a787d8e89aa2e5f8fd6a3998
Junzhi Lu <junzhil@al.com.au>
2025-10-08 23:04:29 -0700

    tabsize=4

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -27,9 +27,9 @@ vim.opt.breakindent = true
 
 -- Tabstops
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 -- Window splitting
 vim.opt.splitright = true
