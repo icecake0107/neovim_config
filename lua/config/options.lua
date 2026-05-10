ce259894df52bf3f392c5599002cebc896b1058b
josh <493302+icecake0107@users.noreply.github.com>
2026-05-10 00:07:36 -0700

    add smart indentation

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -36,12 +36,12 @@ vim.opt.shiftwidth = 4
 
 -- Set tab width to 2 spaces for C/C++ files
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.softtabstop = 2
-    vim.opt_local.shiftwidth = 2
-  end,
+    pattern = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end,
 })
 
 -- Window splitting
@@ -72,25 +72,30 @@ vim.opt.formatoptions:remove { 't' }
 vim.opt.eol = false
 vim.opt.winborder = 'rounded'
 
+-- indentation
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
 })
 
 -- folding
 vim.opt.fillchars = {
-  foldopen = '',
-  foldclose = '',
-  fold = ' ',
-  foldsep = ' ',
-  diff = '╱',
-  eob = ' ',
+    foldopen = '',
+    foldclose = '',
+    fold = ' ',
+    foldsep = ' ',
+    diff = '╱',
+    eob = ' ',
 }
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
